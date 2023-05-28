afcb427c7a04411a4bf6109d3db1bc771a2dd2af
ganeshbarhate2023 <gsbarhate1994@gmail.com>
2023-05-28 18:51:47 +0530

    test

--- vps/src/main/java/com/app/vps/mail/IMailService.java
+++
@@ -1,8 +0,0 @@
-package com.app.vps.mail;
-
-import com.app.vps.exception.ApplicationException;
-
-public interface IMailService {
-	
-	void sendMail(String to,String cc, String[] mailIds, String msgBody, String msgSubject, String from) throws ApplicationException, Exception;
-}
